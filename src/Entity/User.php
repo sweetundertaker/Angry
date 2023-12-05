349d3c3977c9f936b6f4057504d92a3f415047e7
Nikita <nikitalutsenko030796@yandex.ru>
2023-12-05 14:05:39 +0300

    test

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,7 +2,73 @@
 
 namespace App\Entity;
 
-class User
+use App\Repository\ExampleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExampleRepository::class)]
+class User // Имя Сущности
 {
+    //guid
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    //string
+    #[ORM\Column(length: 255)]
+    private ?string $login = null;
+
+    //datetime
+    #[ORM\Column]
+    private ?\DateTimeImmutable $lastLogin = null;
+
+    //string
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+    public function getLogin(): ?int
+    {
+        return $this->login;
+    }
+
+    public function setLogin(string $login): static
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+    public function getLastLogin(): ?\DateTimeImmutable
+    {
+        return $this->lastLogin;
+    }
+
+    public function setLastLogin(\DateTimeImmutable $lastLogin): static
+    {
+        $this->lastLogin = $lastLogin;
+
+        return $this;
+    }
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
 
 }
