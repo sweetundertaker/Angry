e358d346eb9d90934ad90127d3a56aff3a8eaf90
Nikita <nikitalutsenko030796@yandex.ru>
2023-12-05 14:10:51 +0300

    test

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,9 +3,10 @@
 namespace App\Entity;
 
 use App\Repository\ExampleRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: ExampleRepository::class)]
+#[ORM\Entity(repositoryClass: UserRepository::class)]
 class User // Имя Сущности
 {
     //guid
